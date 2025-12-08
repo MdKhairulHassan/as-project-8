e9adce17acc4952646548f3634d1be76a8d55ddc
MdKhairulHassan <darknessmoon76@gmail.com>
2025-12-08 16:36:36 +0600

    final-deployment

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 // import App from './App.jsx';
 import { RouterProvider } from 'react-router/dom';
-import { router } from './Routes/routes';
+import { router } from './Routes/Routes';
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
 
